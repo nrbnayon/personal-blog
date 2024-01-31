135e0d50d9f62bb106595ea06bdced50cd75c39d
nrbnayon <nrbnayon@gmail.com>
2024-01-31 21:00:49 +0600

    MongoDB Connected

--- api/index.js
+++ api/index.js
@@ -1,6 +1,18 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express();
+mongoose
+  .connect(process.env.MONGODB)
+  .then(() => {
+    console.log("My Database is Connected");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 app.listen(3000, () => {
   console.log("Server listening on port 3000");
