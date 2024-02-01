932b831a1c9889b019cea28e1a957dfed8abf94a
nrbnayon <nrbnayon@gmail.com>
2024-02-02 01:54:00 +0600

    Google OAuth Done

--- api/controller/auth.controller.js
+++ api/controller/auth.controller.js
@@ -2,9 +2,9 @@ import bcryptjs from "bcryptjs";
 import jwt from "jsonwebtoken";
 import User from "./../models/user.model.js";
 import { errorHandler } from "../utils/error.js";
+
 export const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
-
   if (
     !username ||
     !email ||
@@ -58,6 +58,52 @@ export const login = async (req, res, next) => {
         httpOnly: true,
       })
       .json(rest);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const google = async (req, res, next) => {
+  const { email, name, googlePhotoUrl } = req.body;
+  try {
+    const user = await User.findOne({ email });
+    if (user) {
+      const token = jwt.sign(
+        {
+          id: user._id,
+        },
+        process.env.JWT_SECRET
+      );
+      const { password: pass, ...rest } = user._doc;
+      res
+        .status(200)
+        .cookie("access_token", token, {
+          httpOnly: true,
+        })
+        .json(rest);
+    } else {
+      const generatedPassword =
+        Math.random().toString(36).slice(-8) +
+        Math.random().toString(36).slice(-8);
+      const hasedPassword = bcryptjs.hashSync(generatedPassword, 10);
+      const newUser = new User({
+        username:
+          name.toLowerCase().split(" ").join("") +
+          Math.random().toString(9).slice(-4),
+        email,
+        password: hasedPassword,
+        profilePicture: googlePhotoUrl,
+      });
+      await newUser.save();
+      const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET);
+      const { password, ...rest } = newUser._doc;
+      res
+        .status(200)
+        .cookie("access_token", token, {
+          httpOnly: true,
+        })
+        .json(rest);
+    }
   } catch (error) {
     next(error);
   }
