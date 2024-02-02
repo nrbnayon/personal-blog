6740eb8f11a8b8b09dd63633df3ab42a0ae3bafb
nrbnayon <nrbnayon@gmail.com>
2024-02-02 12:26:00 +0600

    added header profile popup

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,10 +1,19 @@
-import { Button, Navbar, TextInput } from "flowbite-react";
+import { Avatar, Button, Dropdown, Navbar, TextInput } from "flowbite-react";
 import { Link, useLocation } from "react-router-dom";
 import { AiOutlineSearch } from "react-icons/ai";
 import { FaMoon } from "react-icons/fa";
+import {
+  HiUser,
+  HiCog,
+  HiOutlineBriefcase,
+  HiLogout,
+  HiViewGrid,
+} from "react-icons/hi";
+import { useSelector } from "react-redux";
 
 export default function Header() {
   const path = useLocation().pathname;
+  const { currentUser } = useSelector((state) => state.user);
   return (
     <Navbar className="border-b-2 sticky top-0 z-50">
       <Link
@@ -14,7 +23,7 @@ export default function Header() {
         "
       >
         <span className="text-white px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-400 rounded-lg ">
-          Nayon's
+          Nayon&apos;s
         </span>
         Blog
       </Link>
@@ -33,11 +42,43 @@ export default function Header() {
         <Button className="w-12 h-10 hidden md:inline " color="gray" pill>
           <FaMoon />
         </Button>
-        <Link to="/login">
-          <Button gradientDuoTone="purpleToBlue" outline>
-            Sign In
-          </Button>
-        </Link>
+        {currentUser ? (
+          <Dropdown
+            arrowIcon={false}
+            inline
+            label={
+              <Avatar
+                alt="User"
+                img={currentUser.profilePicture}
+                rounded
+                status="online"
+                statusPosition="top-right"
+              />
+            }
+          >
+            <Dropdown.Header>
+              <span className="block text-sm">@{currentUser.username}</span>
+              <span className="block truncate text-sm font-medium">
+                {currentUser.email}
+              </span>
+            </Dropdown.Header>
+            <Link to={"/dashboard?tab=profile"}>
+              <Dropdown.Item icon={HiUser}>Profile</Dropdown.Item>
+            </Link>
+            <Dropdown.Item icon={HiViewGrid}>Dashboard</Dropdown.Item>
+            <Dropdown.Item icon={HiCog}>Settings</Dropdown.Item>
+            <Dropdown.Item icon={HiOutlineBriefcase}>Projects</Dropdown.Item>
+            <Dropdown.Item icon={FaMoon}>Change Mood</Dropdown.Item>
+            <Dropdown.Divider />
+            <Dropdown.Item icon={HiLogout}>Sign out</Dropdown.Item>
+          </Dropdown>
+        ) : (
+          <Link to="/login">
+            <Button gradientDuoTone="purpleToBlue" outline>
+              Sign In
+            </Button>
+          </Link>
+        )}
         <Navbar.Toggle />
       </div>
       <Navbar.Collapse>
