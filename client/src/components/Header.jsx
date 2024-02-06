e006249ded7d4340284605e0cb3a4c0c20f9af03
nrbnayon <nrbnayon@gmail.com>
2024-02-06 21:39:41 +0600

    Add sign out functionality

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -11,12 +11,29 @@ import {
 } from "react-icons/hi";
 import { toggleTheme } from "../redux/theme/themeSlice";
 import { useSelector, useDispatch } from "react-redux";
+import { signoutSuccess } from "../redux/user/userSlice";
 
 export default function Header() {
   const path = useLocation().pathname;
   const dispatch = useDispatch();
   const { theme } = useSelector((state) => state.theme);
   const { currentUser } = useSelector((state) => state.user);
+
+  const handleSignout = async () => {
+    try {
+      const res = await fetch("/api/user/signout", {
+        method: "POST",
+      });
+      const data = await res.json();
+      if (!res.ok) {
+        console.log(data.message);
+      } else {
+        dispatch(signoutSuccess());
+      }
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
   return (
     <Navbar className="border-b-2 sticky top-0 z-50">
       <Link
@@ -82,7 +99,9 @@ export default function Header() {
               <span className="ml-2">Theme</span>
             </Dropdown.Item>
             <Dropdown.Divider />
-            <Dropdown.Item icon={HiLogout}>Sign out</Dropdown.Item>
+            <Dropdown.Item icon={HiLogout} onClick={handleSignout}>
+              Sign out
+            </Dropdown.Item>
           </Dropdown>
         ) : (
           <Link to="/login">
