0390a27c2043aed0b3e81546e991e7cc06083c2b
nrbnayon <nrbnayon@gmail.com>
2024-02-06 21:39:17 +0600

    Add sign out functionality

--- client/src/components/DashSidebar.jsx
+++ client/src/components/DashSidebar.jsx
@@ -9,10 +9,13 @@ import {
   HiUser,
   HiViewBoards,
 } from "react-icons/hi";
+import { useDispatch } from "react-redux";
+import { signoutSuccess } from "../redux/user/userSlice";
 
 export default function DashSidebar() {
   const location = useLocation();
   const [tab, setTab] = useState("");
+  const dispatch = useDispatch();
   useEffect(() => {
     const urlParams = new URLSearchParams(location.search);
     const tabFromUrl = urlParams.get("tab");
@@ -20,6 +23,22 @@ export default function DashSidebar() {
       setTab(tabFromUrl);
     }
   }, [location.search]);
+
+  const handleSignout = async () => {
+    try {
+      const res = await fetch("/api/user/signout", {
+        method: "POST",
+      });
+      const data = await res.json();
+      if (!res.ok) {
+        console.log(data.message);
+      } else {
+        dispatch(signoutSuccess());
+      }
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
   return (
     <Sidebar className="w-full md:w-56">
       <Sidebar.Items>
@@ -48,7 +67,11 @@ export default function DashSidebar() {
           <Sidebar.Item href="#" icon={HiShoppingBag}>
             Products
           </Sidebar.Item>
-          <Sidebar.Item icon={HiLogout} className="cursor-pointer">
+          <Sidebar.Item
+            icon={HiLogout}
+            className="cursor-pointer"
+            onClick={handleSignout}
+          >
             Sign out
           </Sidebar.Item>
         </Sidebar.ItemGroup>
